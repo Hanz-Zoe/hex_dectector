b598c889128e4ab24bbb6276bc05da1cdbd342c5
Matheus Henrique <mathsmeireles@gmail.com>
2023-06-14 20:51:05 -0300

    feat: refatorando o código

--- main.py
+++ main.py
@@ -1,42 +1,37 @@
 import cv2 as cv
 
-# Reading the image with opencv
+# lendo a imagem
 img = cv.imread('assets/baboon.jpg')
 
 # declarando variáveis globais
-clicked = False
-r = g = b = xpos = ypos = 0
+clicado = False
+r = g = b = 0
 
-# função para obter x, y quando houver clique do mouse
-def draw_function(event, x,y,flags,param):
-    if event == cv.EVENT_LBUTTONDBLCLK:
-        global b,g,r,xpos,ypos, clicked
-        clicked = True
-        xpos = x
-        ypos = y
-        b,g,r = img[y,x]
-        b = int(b)
-        g = int(g)
-        r = int(r)
+# obtendo x e y quando houver duplo clique do mouse em um pixel específico
+def desenhar_cor_na_tela(evento, x, y, flags, param):
+    if evento == cv.EVENT_LBUTTONDBLCLK:
+        global r, g, b, clicado
+        clicado = True
+        b, g, r = img[y,x]
+        r, g, b = int(r), int(g), int(b)
 
 cv.namedWindow('imagem')
-cv.setMouseCallback('imagem',draw_function)
+cv.setMouseCallback('imagem', desenhar_cor_na_tela)
        
 while(1):        
+    cv.imshow('imagem', img)
 
-    cv.imshow("imagem", img)
-
-    if (clicked):
+    if (clicado):
         # cria o retângulo com a cor clicada pelo mouse
         cv.rectangle(img,(20,20), (750,60), (b,g,r), -1)
 
         # retorna o código hexadecimal
-        text = 'cor em hexadecimal' 
+        texto = 'cor em hexadecimal' 
             
         # coloca o texto na tela
-        cv.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv.LINE_AA)
+        cv.putText(img, texto, (50,50), 2, 0.8, (255,255,255), 2, cv.LINE_AA)
 
-        clicked = False
+        clicado = False
 
     # para o loop ao apertar a tecla 'esc'    
     if cv.waitKey(20) & 0xFF == 27:
